28910e1837d6f73e4cc2f14ef1bc8544241e0e50
Erick Valenciae <evalencia0911@gmail.com>
2023-11-02 13:57:57 -0600

    completed all of section 5

--- terminal_homework.bash
+++ terminal_homework.bash
@@ -26,3 +26,46 @@
     3) mv millenium_falcon death_star
     4) mv princess_leia millenium_falcon
     
+    //Part 5: rm - remove
+    1) cd millenium_falcon
+    2) rm ben_kenobi
+
+    //Part 6: all together
+    1) mkdir yavin_4
+    2) cd death_star
+    3) mv millenium_falcon  ..
+    4) cd ..
+    5) mv millenium_falcon yavin_4
+    6) cd yavin_4
+    7) mkdir x_wing
+    8) cd millenium_falcon
+    9) mv luke.txt princess_leia.txt ..
+    10) cd ..
+    11) mv luke.txt x_wing
+    12) mv millenium_falcon x_wing ..
+    13) cd ..
+    14) cd death_star
+    15) mkdir tie_figheter_1 tie_figheter_2 tie_figheter_3
+    16) mv darth_vader.txt tie_figheter_1
+    17) cp storm_trooper tie_figheter_2 tie_figheter_3 //This only added to 3 and I had to do it again and add to 2. Unsure why
+    18) mv tie_figheter_1 tie_figheter_2
+    19) cd ..
+
+    //Part 7: rm -r - remove directories
+    1) rm tie_figheter_2 tie_figheter_3
+    2) rm -r tie_figheter_2 tie_figheter_3
+
+    //Part 8: The Final Act
+    1) cd x_wing
+    2) touch the_force.txt
+    3) cd ..
+    4) rm -r death_star
+    5) mv x_wing yavin_4
+    6) mv millenium_falcon yavin_4
+    //Unsure why: mv x_wing/millenium_falco yavin_4 - didn't work. Looked it up and said you can more 2 directories in that way
+
+
+
+
+
+
